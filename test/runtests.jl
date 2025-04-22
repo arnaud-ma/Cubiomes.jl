c6a650c638c9cfdcf471d0614844a8548b168796
arnaud-ma <arnaudma.code@gmail.com>
2025-04-22 06:23:37 +0200

    fix nightly tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,27 @@
 using Test: @testset
 using Cubiomes
 
-@testset "Aqua" begin
-    include("aqua.jl")
-end
-
-
 if "javarng" in ARGS || "onlyjavarng" in ARGS
     try
         using JavaCall: JavaCall
         JavaCall.init()
     catch e
-        @warn "Something went wrong with Java. Probably it was not found in the system. Skipping JavaRNG tests."
-    else
-        @testset "JavaRNG" begin
-            include("rng.jl")
-        end
+        @warn "Something went wrong with Java. Ignorging the error and continue the tests. \
+         Error: \n $(sprint(showerror, e))"
+    end
+    @testset "JavaRNG" begin
+        include("rng.jl")
     end
     if "onlyjavarng" in ARGS
         exit(0)
     end
 end
+
+@testset "Aqua" begin
+    include("aqua.jl")
+end
+
+
 @testset "Noise" begin
     include("noise.jl")
 end
