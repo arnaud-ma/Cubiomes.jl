872590bef127ebc09322e8aa3651088663a79bb8
arnaud-ma <arnaudma.code@gmail.com>
2025-04-22 05:44:44 +0200

    manage the JULIA_COPY_STACKS = 1 with JavaCall (javarng here)

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,20 @@ using Cubiomes
     include("aqua.jl")
 end
 
-try
-    using JavaCall: JavaCall
-    JavaCall.init()
-catch e
-    @warn "Something went wrong with Java. Probably it was not found in the system. Skipping JavaRNG tests."
-else
-    @testset "JavaRNG" begin
-        include("rng.jl")
+
+if "javarng" in ARGS || "onlyjavarng" in ARGS
+    try
+        using JavaCall: JavaCall
+        JavaCall.init()
+    catch e
+        @warn "Something went wrong with Java. Probably it was not found in the system. Skipping JavaRNG tests."
+    else
+        @testset "JavaRNG" begin
+            include("rng.jl")
+        end
+    end
+    if "onlyjavarng" in ARGS
+        exit(0)
     end
 end
 @testset "Noise" begin
